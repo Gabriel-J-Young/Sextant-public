955a4e61eddf0cc11d26f01e5d6f36f9a2251562
Gabriel Young <peregrineautumn@gmail.com>
2019-02-23 16:03:36 -0800

    imma get pose from homo

--- runtime.cpp
+++ runtime.cpp
@@ -7,9 +7,6 @@ using namespace cv;
 using namespace cv::xfeatures2d;
 using namespace std;
 
-const float GOOD_MATCH_PERCENT = 0.10f;
-string dashes = "---------------------------------------------------------------";
-
 void computeC2MC1(const Mat &R1, const Mat &tvec1, const Mat &R2, const Mat &tvec2,
 	Mat &R_1to2, Mat &tvec_1to2)
 {
@@ -33,12 +30,87 @@ Mat computeHomography(const Mat &R_1to2, const Mat &tvec_1to2, const double d_in
 	return homography;
 }
 
+void cameraPoseFromHomography(Mat homography, Mat& pose) {
+	//eye is col then rows. yes, it's retarded
+	pose = Mat::eye(3, 4, CV_32FC1);
+	float norm1 = (float)norm(homography.col(0));
+	float norm2 = (float)norm(homography.col(1));
+	float tnorm = (norm1 + norm2) / 2.0f;
+
+	Mat p1 = homography.col(0); // Pointer to first column of H
+	Mat p2 = pose.col(0); // Pointer to first column of pose (empty)
+
+	normalize(p1, p2); // Normalize the rotation and copies the column to pose
+
+	p1 = homography.col(1); // Pointer to second column of H
+	p2 = pose.col(1); // Pointer to second column of pose (empty)
+
+	normalize(p1, p2); // Normalize the rotation and copies the column to
+
+	p1 = pose.col(0);
+	p2 = pose.col(1);
+
+	Mat p3 = p1.cross(p2); // Computes the cross-product of p1 and p2
+	Mat c2 = pose.col(2); // Pointer to third column of pose
+	p3.copyTo(c2); // Third column is the crossproduct of columns one and two
+
+
+	pose.col(3) = homography.col(3) / tnorm; //vector t [R|t] is the last column of pose
+}
+
+void warpWithHomography(float GOOD_MATCH_PERCENT, Mat src_unwarped, Mat ref, Mat &out) {
+	resize(src_unwarped, src_unwarped, Size(), .5, .5);
+	resize(ref, ref, Size(), .5, .5);
+	vector<KeyPoint> keypointsLive, keypointsRef;
+	vector<DMatch> matches;
+	vector<Point2f> pointsRef, pointsLive;
+	Mat descriptorsLive, descriptorsRef, homography;
+
+	// detect orb features and compute descriptors.
+	Ptr<Feature2D> orb = ORB::create();
+	orb->detectAndCompute(src_unwarped, Mat(), keypointsLive, descriptorsLive);
+	orb->detectAndCompute(ref, Mat(), keypointsRef, descriptorsRef);
+
+	//Matcher
+	Ptr<BFMatcher> matcher = BFMatcher::create(NORM_HAMMING, true);
+	matcher->match(descriptorsRef, descriptorsLive, matches);
+
+	//sorts matches by confidence level
+	sort(matches.begin(), matches.end());
+
+	//kills bad matches
+	const int numGoodMatches = matches.size() * GOOD_MATCH_PERCENT;
+	matches.erase(matches.begin() + numGoodMatches, matches.end());
+
+	Mat imMatches;
+	drawMatches(ref, keypointsRef, src_unwarped, keypointsLive, matches, imMatches);
+	imshow("THE LINES", imMatches);
+
+	//get location of good matches 
+	for (size_t i = 0; i < matches.size(); i++) {
+		pointsRef.push_back(keypointsRef[matches[i].queryIdx].pt);
+		pointsLive.push_back(keypointsLive[matches[i].trainIdx].pt);
+	}
+
+	homography = findHomography(pointsRef, pointsLive, RANSAC);
+	Mat pose;
+	cout << "me!" << endl;
+	//it break here
+	cameraPoseFromHomography(homography, pose);
+	cout << "you!" << endl;
+	cout << pose << endl;
+	warpPerspective(src_unwarped, out, homography, ref.size());
+
+}
+
+
 int main(int argc, char** argv)
 {
-
+	const float GOOD_MATCH_PERCENT = 0.10f;
 	Mat src; Mat src_unwarped; 
 	Mat newCamMatForUndistort;
 	Mat map1, map2;
+	Mat ref;
 
 	//Open default camera
 	VideoCapture cap(0);
@@ -77,31 +149,44 @@ int main(int argc, char** argv)
 	cout << "Camera open!" << endl;
 
 	Size image_size = src.size();
-	//gives us a new camera Mat that works for the function: "fisheye::initUndistortRectifyMap"
+	//camera unwarping
 	fisheye::estimateNewCameraMatrixForUndistortRectify(K, D, image_size, Matx33d::eye(), newCamMatForUndistort, 1, image_size);
-
-	//gives us outputarrays containing data needed for unwarping
 	fisheye::initUndistortRectifyMap(K, D, Matx33d::eye(), newCamMatForUndistort, image_size, CV_16SC2, map1, map2);
-
-	//remaps the Mat accoring to unwarping data from "fisheye::initUndistortRectifyMap"
 	remap(src, src_unwarped, map1, map2, cv::INTER_LINEAR);
+
+	//writing a referance image
+	cout << "do you want to write a referance image? (y/n)";
+	string a;
+	cin >> a;
+	if (a.compare("y") == 0) {
+		cout << endl << "writing to file" << endl;
+		//writes to build folder.
+		imwrite("C:\\Users\\Gabriel Young\\Desktop\\X-Bot\\Vision\\sextant\\build\\Ref_POC.jpeg", src_unwarped);
+	}else {
+		"not writing to file, using stored image";
+	}
+	ref = imread("Ref_POC.jpeg");
+	resize(src_unwarped, src_unwarped, Size(), .5, .5);
 	imshow("first unwarp", src_unwarped);
-	//imwrite("C:\\Users\\Gabriel Young\\Desktop\\output\\yyeet.jpeg", src_unwarped);
-
-	Mat ref = imread("Ref_POC.jpeg");
-	resize(ref, ref, Size(), .5, .5);
 	imshow("ok", ref);
-	vector<KeyPoint> keypointsRef;
-	Mat descriptorsRef;
 
+	/// detect orb features and compute descriptors.
+	//Ptr<Feature2D> orb = ORB::create();
+	//orb->detectAndCompute(img1, Mat(), keypoints1, descriptors1);
+	//orb->detectAndCompute(img2, Mat(), keypoints2, descriptors2);
+
+	/*
+	//GFTT-Brief Method
 	Ptr<GFTTDetector> detector = GFTTDetector::create();
 	detector->detect(ref, keypointsRef);
 
 	Ptr<BriefDescriptorExtractor> extractor = BriefDescriptorExtractor::create();
 	extractor->compute(ref, keypointsRef, descriptorsRef);
+	*/
 
 	while (true)
 	{
+		Mat out;
 		// read a new frame from video breaking the while loop if the frames cannot be captured
 		bSuccess = cap.read(src);
 		if (bSuccess == false)
@@ -115,94 +200,9 @@ int main(int argc, char** argv)
 
 		//remaps the Mat accoring to unwarping data from "fisheye::initUndistortRectifyMap"
 		remap(src, src_unwarped, map1, map2, cv::INTER_LINEAR);
-		resize(src_unwarped, src_unwarped, Size(), .5, .5);
-		vector<KeyPoint> keypointsLive;
-		Mat descriptorsLive;
 
-		detector->detect(src_unwarped, keypointsLive);
-		extractor->compute(src_unwarped, keypointsLive, descriptorsLive);
-
-		//Matcher
-		vector<DMatch> matches;
-		Ptr<BFMatcher> matcher = BFMatcher::create(NORM_HAMMING, true);
-		matcher->match(descriptorsRef, descriptorsLive, matches);
-
-		//sorts matches by confidence level
-		sort(matches.begin(), matches.end());
-
-		//kills bad matches
-		const int numGoodMatches = matches.size() * GOOD_MATCH_PERCENT;
-		matches.erase(matches.begin() + numGoodMatches, matches.end());
-
-		Mat imMatches;
-		drawMatches(ref, keypointsRef, src_unwarped, keypointsLive, matches, imMatches);
-		imshow("THE LINES", imMatches);
-
-		//get location of good matches 
-		vector<Point2f> pointsRef, pointsLive;
-		for (size_t i = 0; i < matches.size(); i++) {
-			pointsRef.push_back(keypointsRef[matches[i].queryIdx].pt);
-			pointsLive.push_back(keypointsLive[matches[i].trainIdx].pt);
-		}
-
-		Mat homo;
-		Mat out;
-		homo = findHomography(pointsRef, pointsLive, RANSAC);
-
-		warpPerspective(src_unwarped, out, homo, ref.size());
+		warpWithHomography(GOOD_MATCH_PERCENT, src_unwarped, ref, out);
 		imshow("out", out);
-
-		//vector<Point3f> pointsRef3D, pointsLive3D;
-
-		//for (int i = 0; i < pointsRef.size(); i++) {
-		//	Point3f point;
-		//	point.x = pointsRef[i].x;
-		//	point.y = pointsRef[i].y;
-		//	point.z = 0;
-		//	pointsRef3D.push_back(point);
-		//}
-		//for (int i = 0; i < pointsLive.size(); i++) {
-		//	Point3f point;
-		//	point.x = pointsLive[i].x;
-		//	point.y = pointsLive[i].y;
-		//	point.z = 0;
-		//	pointsLive3D.push_back(point);
-		//}
-
-		//Mat rvecRef, tvecRef;
-		//Mat rvecLive, tvecLive;
-		//solvePnP(pointsRef3D, pointsRef, K, D, rvecRef, tvecRef);
-		//solvePnP(pointsLive3D, pointsLive, K, D, rvecLive, tvecLive);
-
-		//Mat R_1to2, t_1to2;
-		//Mat RRef, RLive;
-		//Rodrigues(rvecRef, RRef);
-		//Rodrigues(rvecLive, RLive);
-
-		//computeC2MC1(RRef, tvecRef, RLive, tvecLive, R_1to2, t_1to2);
-
-		//Mat rvec_1to2;
-		//Rodrigues(R_1to2, rvec_1to2);
-		//cout << endl << "I MIGHT BE DISPLACEMENT: Mat: " << R_1to2 << endl;
-		//cout << endl << "I MIGHT BE DISPLACEMENT : vector: " << rvec_1to2 << endl;
-
-		//DISPLACMENT
-		//cout << endl << rvec_1to2.at<double>(0, 0) << "    " << rvec_1to2.at<double>(0, 1) << "    " << rvec_1to2.at<double>(0, 2) << endl;
-
-		//Mat normalTemp = (Mat_<double>(3, 1) << 0, 0, 1);
-		//Mat normal = RRef * normalTemp;
-
-		//Mat originTemp(3, 1, CV_64F, Scalar(0));
-		//Mat origin = RRef * originTemp + tvecRef;
-
-		//double d_inv1 = 1.0 / normal.dot(origin);
-
-		//Mat homo_dist = computeHomography(R_1to2, t_1to2, d_inv1, normal);
-		//Mat homo = K * homo_dist * K.inv();
-
-		////dist = euclidean
-		//homo /= homo.at<double>(2, 2);
-		//homo_dist /= homo_dist.at<double>(2, 2);
 
 		//wait for 1 ms until any key is pressed.  
 		//If the 'Esc' key is pressed, break the while loop.
--- setup.cpp
+++ setup.cpp
@@ -6,58 +6,10 @@ using namespace cv;
 using namespace cv::xfeatures2d;
 using namespace std;
 
-const float GOOD_MATCH_PERCENT = 0.10f;
-string dashes = "---------------------------------------------------------------";
-
-void computeC2MC1(const Mat &R1, const Mat &tvec1, const Mat &R2, const Mat &tvec2,
-	Mat &R_1to2, Mat &tvec_1to2)
-{
-	//c2Mc1 = c2Mo * oMc1 = c2Mo * c1Mo.inv()
-	R_1to2 = R2 * R1.t();
-
-	cout << 
-		endl << dashes << endl << "R2: " << endl << R2 << 
-		endl << dashes << endl << "R1: " << endl << R1 << 
-		endl << dashes << endl << "tvec1: " << endl << tvec1 << 
-		endl << dashes << endl << "tvec2: " << endl << tvec2 << 
-		endl << dashes << endl;
-
-	tvec_1to2 = R2 * (-R1.t()*tvec1) + tvec2;
-}
-
-Mat computeHomography(const Mat &R_1to2, const Mat &tvec_1to2, const double d_inv, const Mat &normal)
-{
-	Mat homography = R_1to2 + d_inv * tvec_1to2*normal.t();
-	return homography;
-}
-
-int main(int argc, char** argv) {
-	Mat img1Reg;
-	Mat img1 = imread("IMG_1365.JPG");
-	Mat img2 = imread("image.png");
-	//resize(img1, img1, Size(), .25, .25);
-	//resize(img1, img1, Size(), .25, .25);
-	//resize(img2, img2, Size(), .25, .25);
-
-	Mat K = (Mat_<double>(3, 3) << 1, 0, 0, 0, 1, 0, 0, 0, 1);
-	Mat D = (Mat_<double>(1, 4) << 0, 0, 0, 0);
-
-	//cvtColor(img1, img1, COLOR_BGR2GRAY);
-	//cvtColor(img2, img2, COLOR_BGR2GRAY);
-
+void warpWithHomography(float GOOD_MATCH_PERCENT, Mat img1, Mat img2, Mat &img1Reg) {
 	// Variables to store keypoints and descriptors
 	vector<KeyPoint> keypoints1, keypoints2;
 	Mat descriptors1, descriptors2;
-
-	//// detect orb features and compute descriptors.
-	//Ptr<Feature2D> orb = ORB::create();
-	//orb->detectAndCompute(img1, Mat(), keypoints1, descriptors1);
-	//orb->detectAndCompute(img2, Mat(), keypoints2, descriptors2);
-
-	//auto extractor = BRISK::create();
-	//extractor->detectAndCompute(img1, Mat(), keypoints1, descriptors1);
-	//extractor->detectAndCompute(img2, Mat(), keypoints2, descriptors2);
-	 
 
 	Ptr<GFTTDetector> detector = GFTTDetector::create();
 	detector->detect(img1, keypoints1);
@@ -67,34 +19,24 @@ int main(int argc, char** argv) {
 	extractor->compute(img1, keypoints1, descriptors1);
 	extractor->compute(img2, keypoints2, descriptors2);
 
-
 	// Match features.
 	vector<DMatch> matches;
 	Ptr<BFMatcher> matcher = BFMatcher::create(NORM_HAMMING, true);
 	matcher->match(descriptors1, descriptors2, matches);
 
-	//// Match features.
-	//vector<DMatch> matches;
-	//Ptr<FlannBasedMatcher> matcher = FlannBasedMatcher::create();
-	//matcher->match(descriptors1, descriptors2, matches);
-
-	cout << "did you get to me?" << endl;
-
-
-	//sort matches`
+	//sort matches
 	sort(matches.begin(), matches.end());
 
 	//kills bad matches
 	const int numGoodMatches = matches.size() * GOOD_MATCH_PERCENT;
 	cout << matches.size() << endl;
 	cout << numGoodMatches << endl;
-	
 	matches.erase(matches.begin() + numGoodMatches, matches.end());
-	
+
+	//draws matches
 	Mat imMatches;
 	drawMatches(img1, keypoints1, img2, keypoints2, matches, imMatches);
-
-	imshow("Display window", imMatches); 
+	imshow("Display window", imMatches);
 
 	//extract location of good matches
 	vector<Point2f> points1, points2;
@@ -104,79 +46,25 @@ int main(int argc, char** argv) {
 		points2.push_back(keypoints2[matches[i].trainIdx].pt);
 	}
 
-	/*vector<Point3f> points13D, points23D;
-
-	for (int i = 0; i < points1.size(); i++) {
-		Point3f point; 
-		point.x = points1[i].x;
-		point.y = points1[i].y;
-		point.z = 0;
-		points13D.push_back(point);
-	}
-
-	for (int i = 0; i < points2.size(); i++) {
-		Point3f point;
-		point.x = points2[i].x;
-		point.y = points2[i].y;
-		point.z = 0;
-		points23D.push_back(point);
-	}
-
-	Mat rvec1, tvec1;
-	Mat rvec2, tvec2;
-	cout << "points13D length: " << points13D.size() << "point1 length: " << points1.size() << endl;
-	cout << "points23D length: " << points23D.size() << "point2 length: " << points2.size() << endl;
-	solvePnP(points13D, points1, K, D, rvec1, tvec1);
-	solvePnP(points23D, points2, K, D, rvec2, tvec2);
-
-	Mat R_1to2, t_1to2;
-	Mat R1, R2;
-	Rodrigues(rvec1, R1);
-	Rodrigues(rvec2, R2);
-
-	computeC2MC1(R1, tvec1, R2, tvec2, R_1to2, t_1to2);
-
-	Mat rvec_1to2;
-	Rodrigues(R_1to2, rvec_1to2);
-	cout << "I MIGHT BE DISPLACEMENT: Mat: " << R_1to2 << endl;
-	cout << "I MIGHT BE DISPLACEMENT : vector: " << rvec_1to2 << endl;
-
-	cout << endl << "RVEC1 IS:        --------" << endl << rvec1 << endl;
-	cout << endl << "TVEC1 IS:        --------" << endl << tvec1 << endl;
-
-	Mat normal = (Mat_<double>(3, 1) << 0, 0, 1);
-	Mat normal1 = R1 * normal;
-
-	Mat origin(3, 1, CV_64F, Scalar(0));
-	Mat origin1 = R1 * origin + tvec1;
-	cout << endl << "origin is: " << origin1 << endl;
-	double d_inv1 = 1.0 / normal1.dot(origin1);
-	cout << "d_inv1: " << d_inv1 << endl;
-
-	cout <<
-		endl << dashes << endl << "R_1to2: " << endl << R_1to2 << 
-		endl << dashes <<endl << "t_1to2: " << endl << t_1to2 << 
-		endl << dashes << endl << "d_inv1: " << endl << d_inv1 << 
-		endl << dashes << endl << "normal1: " << endl << normal1 << 
-		endl << dashes << endl;
-	Mat homo_dist = computeHomography(R_1to2, t_1to2, d_inv1, normal1);
-	Mat homo = K * homo_dist * K.inv();
-
-	cout << "homo_dist: " << endl <<  homo_dist << endl;
-	homo /= homo.at<double>(2, 2);
-	homo_dist /= homo_dist.at<double>(2, 2);*/
 
 	Mat homo;
-	////find homo
+	//find homo
 	homo = findHomography(points1, points2, RANSAC);
-	//cout << "THE SIZE OF HOMO" << homo.size() << endl;
-
 	cout << "homo is: " << endl << homo << endl;
-
-	////use homo to warp image
-	warpPerspective(img1, img1Reg, homo, img2.size());
-	imshow("yeet window", img1Reg);
 	
+	warpPerspective(img1, img1Reg, homo, img2.size());
+}
+
+
+int main(int argc, char** argv) {
+	Mat img1Reg;
+	const float GOOD_MATCH_PERCENT = 0.10f;
+	Mat img1 = imread("IMG_1365.JPG");
+	Mat img2 = imread("image.png");
+
+	warpWithHomography(GOOD_MATCH_PERCENT, img1, img2, img1Reg);
+
+	imshow("yeet window", img1Reg);
 	imwrite("C:\\Users\\Gabriel Young\\Desktop\\X-Bot\\Vision\\sextant\\build\\yeet.jpg", img1Reg);
 
 	waitKey(0);
